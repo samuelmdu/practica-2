0c568b477119a23d26d86f64cf94561ca285861a
Samuel Madrigal <samuelmadrigal9@gmail.com>
2025-08-05 20:32:17 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 });
 
 app.listen(port, () => {
-    console.log('El server se conectó');
+    console.log(`Escuchando en el puerto ${port}`);
 })
 
 app.get('/', (req, res) => {
